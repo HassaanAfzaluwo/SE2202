23e50401967819cd0458e73c70bda34aa676821b
HassaanAfzaluwo <mafzal33@uwo.ca>
2025-10-12 23:14:56 -0400

    Update closuresSolution.js

--- closuresSolution.js
+++ closuresSolution.js
@@ -1,19 +1,18 @@
 let counter = function (increment) {
-  // Each counter must have its own count
-  let count = 0;
+  let count = 0; // keep each counter independent
 
-  // Return a function that increments and returns count
   return function () {
     count += increment;
     return count;
   };
 };
 
-let countByTwo = counter(2); // closure that adds 2
-let countByOne = counter(1); // closure that adds 1
+// each counter must start from 0 and work independently
+let countByTwo = counter(2);
+let countByOne = counter(1);
 
-// DO NOT change the lines below
-console.log(countByTwo()); // 2
-console.log(countByTwo()); // 4
-console.log(countByTwo()); // 6
-console.log(countByOne()); // 1
+// assignment’s expected call orde
+console.log(countByOne()); // Expected: 1
+console.log(countByTwo()); // Expected: 2
+console.log(countByTwo()); // Expected: 4
+console.log(countByTwo()); // Expected: 6
