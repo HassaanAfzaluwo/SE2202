26f97977c6d5c1c42a641a8a1cfcd1389bdd20bb
HassaanAfzaluwo <mafzal33@uwo.ca>
2025-10-27 16:32:07 -0400

    Update pointFunctionSolution.js

--- pointFunctionSolution.js
+++ pointFunctionSolution.js
@@ -22,14 +22,15 @@ function Point(x, y) {
     };
 }
 
-/* Do not change the lines below */
+
 let p1 = new Point(1,5)
 let p2 = new Point(5,7)
 p1.midpoint(p2).print() // expect (3, 6)
 p2.midpoint(p1).print() // expect (3, 6)
 
-/* Do not change the lines below */
+
 let p3 = new Point(0,0)
 let p4 = new Point(4,8)
 p3.scaleTo(p4,0.25).print() // expect (1, 2)
 p4.scaleTo(p3, 0.75).print() // expect (1, 2)
+
